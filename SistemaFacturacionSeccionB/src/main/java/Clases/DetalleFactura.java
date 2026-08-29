d20519ac13cacf15a125c8804b95519504127d7d
Gustavo Méndez <gmendez@probyteg.com>
2026-08-29 12:04:50 -0600

    Se agrega detalle de la factura

--- SistemaFacturacionSeccionB/src/main/java/Clases/DetalleFactura.java
+++ SistemaFacturacionSeccionB/src/main/java/Clases/DetalleFactura.java
@@ -3,5 +3,27 @@ package Clases;
 
 
 public class DetalleFactura {
+    int idDetalle;
+    String descripcionProducto;
+    int cantidad;
+    double precioUnitario;
+    
+    public DetalleFactura(int idDetalle, String descripcionProducto, int cantidad, double precioUnitario) {
+        this.idDetalle = idDetalle;
+        this.descripcionProducto = descripcionProducto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double calcularSubtotal() {
+        return cantidad * precioUnitario;
+    }
+    
+    public void mostrarDetalle() {
+        System.out.println("Producto: " + descripcionProducto + 
+                           " | Cantidad: " + cantidad + 
+                           " | Precio: Q" + precioUnitario + 
+                           " | Subtotal: Q" + calcularSubtotal());
+    }
     
 }
